119d07b222074519b07f3c349a4ac993a48c9ef4
Shashank Kapsime <shashankkapsime270@gmail.com>
2016-10-08 20:30:48 +0530

    Also mentioned POST rest API integration example

--- WebServiceSample/app/src/main/java/com/webservicesample/MainActivity.java
+++ WebServiceSample/app/src/main/java/com/webservicesample/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.webservicesample.parsers.ServiceResponse;
 import com.webservicesample.webserviceutils.WebServiceAsync;
 import com.webservicesample.webserviceutils.WebServiceStatus;
@@ -33,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     * Here we are using a google geocoder API to see how we can integrate rest API and parse
      * data using GSON library.
     * */
+
+    // This is a GET type of rest API.
     private void callAnAPI() {
         String googleGeocoderURL = "http://maps.google.com/maps/api/geocode/json?latlng=28.525354,77.284634";
         Log.e(TAG, " **googleGeocoderURL** " + googleGeocoderURL);
@@ -51,6 +54,37 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        serviceAsync.execute("");
+    }
+
+    /*
+    * Here for example we have to send email and password as Json Request.
+    * */
+    // This is a POST type of rest API.
+    private void callPostAPI() {
+
+        // It is sample URL, please change it.
+        String googleGeocoderURL = "http://xyz.com";
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("email","asd@das.com"); // key and value
+        jsonObject.addProperty("password","llllllll"); // key and value
+        Log.e(TAG, " **googleGeocoderURL** " + googleGeocoderURL);
+        WebServiceAsync serviceAsync = new WebServiceAsync(mContext, jsonObject.toString(), googleGeocoderURL, "POST", true, new WebServiceStatus() {
+            @Override
+            public void onSuccess(Object o) {
+                ServiceResponse response = new Gson().fromJson(o.toString(), ServiceResponse.class);
+                if (response != null) {
+
+                    // here we can parse the response
+                }
+            }
+
+            @Override
+            public void onFailed(Object o) {
+
+            }
+        });
+        serviceAsync.execute("");
     }
 
     private boolean isOnline(Context context) {
